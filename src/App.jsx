902252578934c80f672fdfe4889050d2914565f0
Victor Noé  Becerra Hernández <victor.becerra@aiscs.com.mx>
2024-09-28 00:15:04 -0600

    Initial commit for portafolio-main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+// src/components/Home.jsx
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is where you will find my work and skills.</p>
+    </div>
+  );
+};
+
+export default Home;
